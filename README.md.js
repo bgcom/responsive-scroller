540691c6c3050b227bf71b78d292d49f6e7cf64b
kevin wenger <wenger.kev@gmail.com>
2012-10-16 13:55:24 +0200

    First Commit

---
+++ README.md.js
@@ -0,0 +1,49 @@
+# FancyScroll - jQuery Plugin
+http://bgcom.ch/ Copyright (c) 2012 B+G & Partners | created by Kevin Wenger - wenger.kev(at)gmail(dot)com
+
+## General Notes
+In an effort to move the plugin forward, support for old jQuery version has been dropped.
+
+Your old styles and properties *might not work out of the box*. Some property names have been changed, noted below, as well as namespacing prefixes being applied to all elements. This means that `.flex-direction-nav .next` is now `.flex-direction-nav .flex-next` by default. The namespacing property is exposed, free for you to change.
+
+## Updates
+Empty
+
+## Examples
+- [Basic](http://mymetropole.ch/)
+
+## Properties
+
+### activeClass: *{new}*
+`activeClass` allow you to change the active class you use.*
+
+### elemActivate: *{new}*
+Which element you want to active with `activeClass`.*
+
+### debug: *{new}*
+`debug` verbose mode.*
+
+### scroll: *{new}*
+#### active: *{new}*
+`active` allow you to active (or not) this functionnality.*
+
+#### offset: *{new}*
+`offset` use to adjuste your scrolling animation .*
+
+#### axis: *{new}*
+`axis` allow you to choose the axis of scrolling animation.*
+
+#### duration: *{new}*
+`duration` allow you to choose the duration of scrolling animation.*
+
+### spy: *{new}*
+#### active: *{new}*
+`active` allow you to active (or not) this functionnality.*
+
+### added: *{new}*
+Empty
+### removed: *{new}*
+Empty
+
+## Contributors
+- [Kevin Wenger](https://github.com/Sudei) 
---
+++ jquery.fancyScroll.js
@@ -0,0 +1,167 @@
+/*
+ * FancyScroll - jQuery Plugin
+ *
+ *
+ * Copyright (c) 2012 B+G & Partners - http://bgcom.ch/ | created by Kevin Wenger - wenger.kev(at)gmail(dot)com
+ * Dual licensed under the MIT and GPL licenses.
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ *
+ * @author Kevin Wenger
+ * @version 1.0 (2012-10-05)
+ * Requires 
+ *   JQuery 1.7.1 - http://jquery.com/
+ *   ScrollTo 1.4.3.1 - http://flesler.blogspot.ch/2007/10/jqueryscrollto.html
+ */
+(function($){
+	$.fancyScroll = function (element, options){
+		context = this;
+		context.options = {};
+		context.percents = new Array();
+		context.blocks = [];
+		context.oldScrollTop = 0;
+
+		element.data('fancyScroll', this);
+
+		this.init = function(element, options) {         
+			this.options = $.extend(true, $.fancyScroll.defaultOptions, options);
+			context.oldScrollTop = $(window).scrollTop();
+			
+			$.each(element.find(context.options.elemActivate + ' a'), function(index, item){
+		    context.blocks.push([$($(item).attr('href')), $(item)]);
+		  });
+				
+			if(this.options.spy.active){
+				spyScroll(this, element);
+			}
+			if(this.options.scroll.active)
+				smoothScroll(this, element);
+			 
+		};
+
+        this.init(element, options);
+	};
+	
+	$.fn.fancyScroll = function(options) {                   
+		return this.each(function() {
+			(new $.fancyScroll($(this), options));              
+		});        
+	};
+	
+	// Activate spy to scroll
+	function spyScroll(context, element){
+		$(window).scroll(function (e) {
+				if ($(window).scrollTop() != context.oldScrollTop){
+					context.percents = new Array();
+					topWindow = $(window).scrollTop() + 60 ; 
+					pageHeight = $(document).height(); 
+					windowHeight = $(window).height(); 
+					footerWindow = topWindow + windowHeight;
+					
+					$.each(context.blocks, function(index, block){
+						percentBlock = 0;
+						topBlock = block[0].offset().top;
+						heightBlock = block[0].height();
+						footerBlock = topBlock + heightBlock;
+						
+						if(topWindow <= topBlock && footerWindow >= topBlock && footerWindow <= footerBlock){
+							inside = footerWindow - topBlock;
+							percentBlock = (inside / heightBlock ) * 100;
+						}
+						else if(footerWindow >= footerBlock && topWindow >= topBlock && topWindow <= footerBlock){
+							outside = topWindow - topBlock;
+							inside = heightBlock - outside;
+							percentBlock = (inside / heightBlock ) * 100 ;
+						}
+						else if(topWindow >= topBlock && topWindow <= footerBlock && footerWindow >= topBlock &&  footerWindow <= footerBlock ){
+							inside = windowHeight;
+							percentBlock = (inside / heightBlock ) * 100 ;
+						}
+						else if(topWindow <= topBlock && footerWindow >= footerBlock){
+							percentBlock = 100;
+						}
+						
+						context.percents.push([block[1], percentBlock]);								
+					});
+					
+					activate(context, element);
+					
+					if(context.options.debug)
+						debugMode(context);			
+				} 
+		});
+	};
+	
+	// Show dialog
+	function smoothScroll(context, element){
+		$(element).on('click', element.find(context.options.elemActivate + ' a'), function(e){
+			var event = e;
+			event.preventDefault();
+			
+			if(context.options.scroll.active)
+				$(window).unbind("scroll");
+									
+			$(document).scrollTo('#' + e.target.href.split('#')[1], context.options.scroll.duration, {axis: context.options.scroll.axis, offset: context.options.scroll.offset, onAfter: function() {          
+							context.oldScrollTop = $(window).scrollTop();
+
+						if(context.options.scroll.active)
+							spyScroll(context, element);
+							
+							var target = event.srcElement || event.target;
+							element.find(context.options.elemActivate).removeClass(context.options.activeClass);
+							addActiveClass($(target).parents(context.options.elemActivate), context);
+					}
+      });			
+
+			return false;	
+		});
+
+	};
+	
+	// activate in menu
+	function activate(context, element){
+	  element.find(context.options.elemActivate).removeClass(context.options.activeClass);
+		maxPercent = 0;
+		selector = '';
+		if($(window).scrollTop() == 0)
+			selector = context.percents[0][0];
+		else if($(window).scrollTop() + $(window).height() == $(document).height())
+			selector = context.percents[context.percents.length - 1][0];
+		else{
+			$.each(context.percents, function(index, percent){
+			    if(percent[1] > maxPercent){
+			    	maxPercent = percent[1];
+			    	selector = percent[0];
+			    }
+			});
+		}
+		addActiveClass(selector.parents(context.options.elemActivate), context);
+	};
+	
+	function addActiveClass(element, context){
+		element.addClass(context.options.activeClass);
+	}
+	
+	function debugMode(context){
+		$.each(context.percents, function(index, percent){
+		    console.log(percent[0].selector + ' : ' + percent[1])
+		}); 
+	};
+	
+	//set default options
+	$.fancyScroll.defaultOptions = {
+		activeClass: "active",
+		elemActivate: "li",
+		debug: false,
+		scroll: {
+			active: true,
+			offset: 0,
+			axis: 'y',
+			duration: 1000
+		},
+		spy:{
+			active: true
+		}
+  }
+	
+})(jQuery);
